b34a94bbddc8d6683fa0d8e83b791dd1f788ad2f
strcodingax <aiden.xue28@ycdsbk12.ca>
2026-09-22 15:01:13 +0000

    added last 2.3 program

---
+++ your_schedule.py
@@ -0,0 +1,34 @@
+course_1_name = str("English: Understanding Contemporary First Nations, Métis, and Inuit Voices")
+course_1_teacher = str("???")
+
+course_2_name = str("World Religions: Beliefs, Issues, and Religious Traditions")
+course_2_teacher = str("???")
+
+course_3_name = str("Grade 11 Functions")
+course_3_teacher = str("Jason Bisogno")
+
+course_4_name = str("Grade 11 Music")
+course_4_teacher = str("Beth Facchini")
+
+course_5_name = str("Introduction to Computer Science")
+course_5_teacher = str("Daniel Gallo")
+
+course_6_name = str("Advanced Functions")
+course_6_teacher = str("???")
+
+course_7_name = str("Grade 11 Physics")
+course_7_teacher = str("Stefano Di Carlo")
+
+course_8_name = str("Grade 11 Chemistry")
+course_8_teacher = str("???")
+
+print("+" + 109 * "-" + "+")
+print("| 1 | {0:>80} | {1:>20} |".format(course_1_name, course_1_teacher))
+print("| 2 | {0:>80} | {1:>20} |".format(course_2_name, course_2_teacher))
+print("| 3 | {0:>80} | {1:>20} |".format(course_3_name, course_3_teacher))
+print("| 4 | {0:>80} | {1:>20} |".format(course_4_name, course_4_teacher))
+print("| 5 | {0:>80} | {1:>20} |".format(course_5_name, course_5_teacher))
+print("| 6 | {0:>80} | {1:>20} |".format(course_6_name, course_6_teacher))
+print("| 7 | {0:>80} | {1:>20} |".format(course_7_name, course_7_teacher))
+print("| 8 | {0:>80} | {1:>20} |".format(course_8_name, course_8_teacher))
+print("+" + 109 * "-" + "+")

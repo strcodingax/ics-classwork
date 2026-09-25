acddd693657c6d99b0539da300d1d2a04f1f01db
strcodingax <aiden.xue28@ycdsbk12.ca>
2026-09-25 10:44:46 -0400

    Added new program for 2.5

---
+++ forgetful_machine.py
@@ -0,0 +1,15 @@
+print("Give me a word!")
+input()
+print("Give me a second word!")
+input()
+
+print("")
+
+print("Great, now your favorite number?")
+input()
+print("And your second-favorite number...")
+input()
+
+print("")
+
+print("Whew! Wasn't that fun?")

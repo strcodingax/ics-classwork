745103b8230820e5b0e181a5fdc4243217d7b249
strcodingax <aiden.xue28@ycdsbk12.ca>
2026-09-25 10:59:59 -0400

    Added the third new program for 2.5
    The first one was user_input by the way

---
+++ name_age_salary.py
@@ -0,0 +1,11 @@
+print("Hello. What is your name?")
+name = input()
+
+str(print("Hi, " + name + "! " + "How old are you?"))
+age = int(input())
+
+print(f"So you're {age}, eh? That's not old at all!")
+print(f"How much do you make, {name}?")
+salary = float(input())
+
+print(f"{round(salary, 1)}! I hope that's per hour and not per year! LOL!")
